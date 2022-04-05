3665e0879eaf9e5d41680ec7234d3ee4ed82176d
cyrusn <cyrusncy@gmail.com>
2022-04-05 18:19:24 +0800

    add prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? "/react-game-of-15/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
